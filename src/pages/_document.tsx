8bb444961a3fbd9add50b91ff43e79e7f02b8f06
Ragon Lyu <ragon.lyu@gmail.com>
2023-08-15 20:25:44 +0800

    feat: word count

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>IELTS Catalyst</title>
-      </Head>
       <body>
+        <Head ></Head>
         <Main />
         <NextScript />
       </body>
